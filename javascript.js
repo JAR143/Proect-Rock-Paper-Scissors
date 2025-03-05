e04ae0b6b61373b13bc17a0b38d8f3d8a67c3ce2
DrowsyChef <radlleyjoe12@gmail.com>
2025-03-06 05:57:24 +0800

    adding score system

--- javascript.js
+++ javascript.js
@@ -19,22 +19,32 @@ function getHumanChoice () {
     return choice
 }
 
+let humanScore = 0;
+let computerScore =0;
+
+function playRound (humanChoice, computerChoice) {
+    
+    if (
+        humanSelection == "Rock" && computerSelection == "Rock" ||
+        humanSelection == "Paper" && computerSelection == "Paper" ||
+        humanSelection == "Scissor" && computerSelection == "Scissor"
+    ) {
+        console.log("It's a tie!");
+    } else if (
+        humanSelection == "Rock" && computerSelection == "Scissor" ||
+        humanSelection == "Paper" && computerSelection == "Rock" ||
+        humanSelection == "Scissor" && computerSelection == "Paper" 
+    ) {
+        console.log(`Yeayy! you win ${humanSelection} beats ${computerSelection}`);
+        console.log("Player: " + ++humanScore);
+    } else {
+        console.log(`You lose. ${computerSelection} beats ${humanSelection}`);
+        console.log("Computer: " + ++computerScore);
+    }
+
+}
+
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-
-if (
-    humanSelection == "Rock" && computerSelection == "Rock" ||
-    humanSelection == "Paper" && computerSelection == "Paper" ||
-    humanSelection == "Scissor" && computerSelection == "Scissor"
-) {
-    console.log("It's a tie!");
-} else if (
-    humanSelection == "Rock" && computerSelection == "Scissor" ||
-    humanSelection == "Paper" && computerSelection == "Rock" ||
-    humanSelection == "Scissor" && computerSelection == "Paper" 
-) {
-    console.log(`Yeayy! you win ${humanSelection} beats ${computerSelection}`);
-} else {
-    console.log(`You lose. ${computerSelection} beats ${humanSelection}`);
-}
+playRound(humanSelection, computerSelection);
